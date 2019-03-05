bdf3745b3c3ef6ea1005c56d4759ae51e14d9860
Anysia V <anysiavalko@gmail.com>
2019-03-05 22:42:45 +0000

    working on cli

--- lib/birthstones/cli.rb
+++ lib/birthstones/cli.rb
@@ -3,12 +3,20 @@ class Birthstones::CLI
     list_birthstones
     main_menu
   end
-end
+
 
   def list_birthstones
-    puts "Every month has its own special birthstone. Below is a list of each month's birthstone.\nIf you would like to find out more about a particular birthstone, please enter just the number of month. For example, to view July's birthstone, type '7'."
+    puts "Birthstones by Month:"
     @stones = Birthstones::Stone.list
     @stones.each_with_index(1) do |stone, i|
       puts "#{i}. #{stone.month} - #{stone.name}"
     end
   end
+
+  def main_menu
+    input = nil
+    while input != "exit"
+      puts puts "Every month has its own special birthstone. Below is a list of each month's birthstone.\nIf you would like to find out more about a particular birthstone, please enter just the number of month. For example, to view July's birthstone, type '7'."
+  end
+
+end
