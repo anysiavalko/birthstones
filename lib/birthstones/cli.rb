e5caf83d7b7ae4b73df42f844eab089f5061fd59
Anysia V <anysiavalko@gmail.com>
2019-03-05 20:23:29 +0000

    test

--- lib/birthstones/cli.rb
+++ lib/birthstones/cli.rb
@@ -1,5 +1,10 @@
 class Birthstones::CLI
   def call
-    
+    list_birthstones
+    main_menu
   end
 end
+
+  def list_birthstones
+    puts "Every month has its own special birthstone. Below is a list of each month's birthstone.\nIf you would like to find out more about a particular birthstone, please enter just the number of month. For example, to view July's birthstone, type '7'."
+  end
