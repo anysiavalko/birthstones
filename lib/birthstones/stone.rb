11a349840225db12628a255c509779b3edf58da3
Anysia V <anysiavalko@gmail.com>
2019-03-06 16:55:14 +0000

    adding stuff to stone file

--- lib/birthstones/stone.rb
+++ lib/birthstones/stone.rb
@@ -1,9 +1,26 @@
 class Birthstones::Stone
-  attr_accessor :name
+  attr_accessor :name, :month
 
   def self.list
     self.scrape_stones
   end
+
+  def self.scrape_stones
+    stones = []
+    stones << self.scrape_gia
+    stones
+  end
+
+  def self.scrape_gia
+    doc = Nokogiri::HTML(open(""))
+
+    stone = self.new
+    stone.name =
+    stone.month =
+
+    stone
+  end
+
 
 
 end
