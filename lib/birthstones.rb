f9b143d6ee4c68bd38c7caf0ab1828945be30cc4
Anysia V <anysiavalko@gmail.com>
2019-03-06 16:34:09 +0000

    trying to imitate other peoples requirements

--- lib/birthstones.rb
+++ lib/birthstones.rb
@@ -1,4 +1,11 @@
-require "birthstones/version"
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+require_relative "./birthstones/version"
+require_relative "./birthstones/stone"
+require_relative "./birthstones/cli"
+require_relative "./birthstones/scraper"
 
 module Birthstones
   class Error < StandardError; end
