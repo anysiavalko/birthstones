9f8f93250cc7be73f385a317cead938577a2aa38
Anysia V <anysiavalko@gmail.com>
2019-03-07 22:24:59 +0000

    returned stone at end of scrape method

--- lib/birthstones/stone.rb
+++ lib/birthstones/stone.rb
@@ -14,11 +14,14 @@ class Birthstones::Stone
 
   def self.scrape_gia
     doc = Nokogiri::HTML(open("https://www.gia.edu/birthstones"))
+
     stone = self.new
     stone.name = doc.css("div.grey").css("p strong").first.text.capitalize
     stone.month = doc.css("div.grey").css("div.title").first.text
     stone.overview = doc.css("div.grey").css("p.facts").first.text
     stone.learn_more = doc.css("div.col-md-4 a").attr("href").value
+
+    stone
   end
 
 
