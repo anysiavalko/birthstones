25274fa4684914403f15a8faa804425cab177122
Anysia V <anysiavalko@gmail.com>
2019-03-08 18:11:34 +0000

    crying

--- lib/birthstones/scraper.rb
+++ lib/birthstones/scraper.rb
@@ -5,8 +5,15 @@ class Birthstones::Scraper
   def self.scrape_page("https://www.gia.edu/birthstones")
     html = open("https://www.gia.edu/birthstones")
     doc = Nokogiri::HTML(html)
+  end
+
+#I have NO Idea WHAT I AM DOING
 
 
+  def create_birthstones
+    scrape_page.each do |stone|
+      Birthstones::Stone.new_from_index_page
+    end
   end
 
 
--- lib/birthstones/stone.rb
+++ lib/birthstones/stone.rb
@@ -2,6 +2,26 @@ require 'pry'
 class Birthstones::Stone
   attr_accessor :name, :month, :overview, :learn_more
 
+  @@all = []
+
+  def self.new_from_gia(url)
+    stone = self.new
+
+    stone
+  end
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
   def self.list
     self.scrape_stones
   end
