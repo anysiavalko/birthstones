fb24a9df7e21697f618b1776575e7292fb4cc43e
Anysia V <anysiavalko@gmail.com>
2019-03-26 21:43:57 +0000

    trying to get only 12 months with a while loop counter thing

--- lib/birthstones/scraper.rb
+++ lib/birthstones/scraper.rb
@@ -6,13 +6,16 @@ class Birthstones::Scraper
     website = Nokogiri::HTML(open("https://www.gia.edu/birthstones"))
     section = website.css("div.gem-library").css("div.grey")
     stones = section.css("div.row")
-    binding.pry
-    section.each do |stone|
-      stone = Birthstones::Stone.new
-      stone.name = stones.css("strong").text.capitalize
-      stone.month = stones.css("div.title").text
-      stone.overview = stones.css("p.facts").text
-      stone.learn_more = stones.css("a").attr("href").value
+    i = 0
+    while i < 12
+      section.each do |stone|
+        stone = Birthstones::Stone.new
+        stone.name = stones.css("strong").text.capitalize
+        stone.month = stones.css("div.title").text
+        stone.overview = stones.css("p.facts").text
+        stone.learn_more = stones.css("a").attr("href").value
+        i += 1
+      end
     end
     # Birthstones::Stone.new
   end
