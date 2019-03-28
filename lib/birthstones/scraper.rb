754a87c96d7380a99788cad29ffc8239a9679af2
Anysia V <anysiavalko@gmail.com>
2019-03-28 20:58:28 +0000

    making changes recommended by JH

--- lib/birthstones/scraper.rb
+++ lib/birthstones/scraper.rb
@@ -5,17 +5,13 @@ class Birthstones::Scraper
   def self.scrape_gia
     website = Nokogiri::HTML(open("https://www.gia.edu/birthstones"))
     section = website.css("div.gem-library").css("div.grey")
-    stones = section.css("div.row")
-    i = 0
-    while i < 12
-      section.each do |stone|
-        stone = Birthstones::Stone.new
-        stone.name = stones.css("strong").text.capitalize
-        stone.month = stones.css("div.title").text
-        stone.overview = stones.css("p.facts").text
-        stone.learn_more = stones.css("a").attr("href").value
-        i += 1
-      end
+    gemstone = section.css("div.row")
+    section.each do |stone|
+      stone = Birthstones::Stone.new
+      stone.name = gemstone.css("strong").text.capitalize
+      stone.month = gemstone.css("div.title").text
+      stone.overview = gemstone.css("p.facts").text
+      stone.learn_more = gemstone.css("a").attr("href").value
     end
     # Birthstones::Stone.new
   end
